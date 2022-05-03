4767cc406743afa8f6610e75a3ca65880a6ed987
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-05-03 15:18:00 +0200

    Test

---
+++ convert_list.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+
+wget https://raw.githubusercontent.com/publicsuffix/list/master/public_suffix_list.dat
+
+while read -r tld; do
+
+if [ -n "$tld" ]; then
+    if ! echo "$tld" | grep '//' > /dev/null; then
+        idn2 --quiet "$tld" >> public_suffix_ascii_list.new.dat 
+    fi
+fi
+if  echo "$tld" | grep '// ===END ICANN DOMAINS===' > /dev/null; then
+    break
+fi
+done < public_suffix_list.dat
+
+if [ -f "public_suffix_ascii_list.dat" ]; then
+    touch public_suffix_ascii_list.dat
+fi
+
+if diff public_suffix_ascii_list.dat public_suffix_ascii_list.new.dat; then
+    rm  public_suffix_ascii_list.dat
+    mv  public_suffix_ascii_list.new.dat public_suffix_ascii_list.dat
+    git addd public_suffix_ascii_list.dat
+    git commit -m "Update Public suffix list"
+    git push
+fi 
+
+rm public_suffix_ascii_list.new.dat
+rm public_suffix_list.dat
