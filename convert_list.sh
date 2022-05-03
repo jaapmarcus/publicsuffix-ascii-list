a6ff099a37964a2e0a94cfccbcf3c29d521fd445
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-05-03 16:24:35 +0200

    Update

--- convert_list.sh
+++ convert_list.sh
@@ -4,13 +4,15 @@ apt-get install git idn2 wget -y
 wget https://raw.githubusercontent.com/publicsuffix/list/master/public_suffix_list.dat
 CHARSET="UTF-8"
 export LC_ALL=en_US.UTF-8
+stop=0;
 while read -r tld; do
 
 if [ -n "$tld" ]; then
     if ! echo "$tld" | grep '//' > /dev/null; then
         echo "$tld"
         out=$(idn2 $tld)
-        echo $out >> public_suffix_ascii_list.new.dat 
+        echo "$out";
+        echo "$out" >> public_suffix_ascii_list.new.dat 
     fi
 fi
 if  echo "$tld" | grep 'END ICANN DOMAINS' > /dev/null; then
@@ -23,12 +25,11 @@ if [ -f "public_suffix_ascii_list.dat" ]; then
     touch public_suffix_ascii_list.dat
 fi
 
-if diff public_suffix_ascii_list.dat public_suffix_ascii_list.new.dat; then
-    rm  public_suffix_ascii_list.dat
-    mv  public_suffix_ascii_list.new.dat public_suffix_ascii_list.dat
-    git add public_suffix_ascii_list.dat
-    git commit -m "Update Public suffix list"
-    git push
+diff -q $dst $new public_suffix_ascii_list.dat public_suffix_ascii_list.new.dat > /dev/null
+if [ $? ]; then
+    echo "Commit changes"
+    rm public_suffix_ascii_list.dat 
+    mv public_suffix_ascii_list.new.dat public_suffix_ascii_list.dat
 fi 
 
 rm public_suffix_ascii_list.new.dat
